ce672353c9929cb2b80b78aad85e5ada85b3e0b0
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-03-06 18:29:29 -0400

    Add eslint to project
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- events.js
+++ events.js
@@ -1,49 +1,53 @@
 const axios = require("axios");
 
 const EVENTS_ENDPOINT =
-  "https://gameinfo.albiononline.com/api/gameinfo/events?limit=51&offset=0";
+    "https://gameinfo.albiononline.com/api/gameinfo/events?limit=51&offset=0";
 
 let lastEventId = null;
 function parse(events, callback) {
-  console.log(`Events fetched: ${events.length} events.`);
-  const newEvents = [];
-  events.every(event => {
-    if (event.EventId === lastEventId) return false;
-    // Ignore Arena kills or Duel kills
-    if (event.TotalVictimKillFame <= 0) {
-      return true;
+    const newEvents = [];
+    let scanned = 0;
+    events.every(event => {
+        if (event.EventId === lastEventId) {
+            return false;
+        }
+        // Ignore Arena kills or Duel kills
+        if (event.TotalVictimKillFame <= 0) {
+            return true;
+        }
+        scanned++;
+
+        // TODO: Feature to check for tracked players / guilds / alliances
+        // Check for kill in event.Killer / event.Victim
+        if (
+            event.Killer.GuildName === "Black River" ||
+            event.Victim.GuildName === "Black River"
+        ) {
+            // Since we are parsing from newer to older events
+            // we need to use FILO array
+            newEvents.unshift(event);
+        }
+
+        return true;
+    });
+    console.log(`Events scanned: ${scanned} events.`);
+
+    if (events.length > 0) {
+        lastEventId = events[0].EventId;
     }
 
-    // TODO: Feature to check for tracked players / guilds / alliances
-    // Check for kill in event.Killer / event.Victim
-    if (
-      event.Killer.GuildName === "Black River" ||
-      event.Victim.GuildName === "Black River"
-    ) {
-      // Since we are parsing from newer to older events
-      // we need to use FILO array
-      newEvents.unshift(event);
+    if (newEvents.length > 0) {
+        console.log(`New events: ${newEvents.length}.`);
+        callback(newEvents);
     }
-
-    return true;
-  });
-
-  if (events.length > 0) {
-    lastEventId = events[0].EventId;
-  }
-
-  if (newEvents.length > 0) {
-    console.log(`New events: ${newEvents.length}.`);
-    callback(newEvents);
-  }
 }
 
 exports.fetch = async callback => {
-  console.log("Fetching Albion Online events...");
-  try {
-    const res = await axios.get(EVENTS_ENDPOINT);
-    parse(res.data, callback);
-  } catch (err) {
-    console.error(`Unable to fetch data from API: ${err}`);
-  }
+    console.log("Fetching Albion Online events...");
+    try {
+        const res = await axios.get(EVENTS_ENDPOINT);
+        parse(res.data, callback);
+    } catch (err) {
+        console.error(`Unable to fetch data from API: ${err}`);
+    }
 };
--- index.js
+++ index.js
@@ -4,33 +4,33 @@ const messages = require("./messages");
 
 const token = process.env.TOKEN;
 if (!token) {
-  console.log(
-    "Please define TOKEN environment variable with the discord token."
-  );
-  process.exit(0);
+    console.log(
+        "Please define TOKEN environment variable with the discord token."
+    );
+    process.exit(0);
 }
 
 const client = new Discord.Client();
 const notify = events => {
-  // TODO: Configure channel
-  const channel = client.channels.find(c => c.name === 'geral');
+    // TODO: Configure channel
+    const channel = client.channels.find(c => c.name === "geral");
 
-  events.forEach(event => {
-    const embed = messages.embed(event);
+    events.forEach(event => {
+        const embed = messages.embed(event);
 
-    // TODO: Send messages to channels based on tracking settings
-    channel.send({
-      embed
+        // TODO: Send messages to channels based on tracking settings
+        channel.send({
+            embed
+        });
     });
-  });
 };
 client.on("ready", () => {
-  console.log(`Connected successfully as ${client.user.tag}`);
-  setInterval(() => eventHandler.fetch(notify), 30000);
+    console.log(`Connected successfully as ${client.user.tag}`);
+    setInterval(() => eventHandler.fetch(notify), 30000);
 });
 client.on("message", msg => {
-  if (msg.content === 'test') {
-    eventHandler.fetch(notify);
+    if (msg.content === "test") {
+        eventHandler.fetch(notify);
     }
 });
 client.login(token);
