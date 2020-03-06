1a8301aef0d7f6158e9557c3886112f9d90b0890
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-03-06 18:29:33 -0400

    Add untrack command
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- commands/untrack.js
+++ commands/untrack.js
@@ -1,13 +1,12 @@
-const { search } = require("../queries/search");
 const { setConfig } = require("../config");
 const { getI18n } = require("../messages");
 
 const SUPPORTED_TYPES = ["player", "guild", "alliance"];
 
 module.exports = {
-  aliases: ["track"],
+  aliases: ["untrack"],
   args: ["player/guild/alliance", "name"],
-  description: "HELP.TRACK",
+  description: "HELP.UNTRACK",
   run: async (client, guild, message, args) => {
     const l = getI18n(guild);
 
@@ -23,67 +22,46 @@ module.exports = {
     const type = args.shift().toLowerCase();
     const name = args.join(" ").trim();
 
-    if (type === "alliance") {
-      message.channel.send(l.__("TRACK.ALLIANCE_DISABLED"));
-      return;
-    }
-
     if (!SUPPORTED_TYPES.includes(type)) {
       message.channel.send(l.__("TRACK.UNSUPPORTED_TYPE"));
       return;
     }
 
-    // TODO: Constantize limits
-    const track = async (entityList = [], dest, msg, limit = 5) => {
-      let entity = dest.find(
+    const untrack = async (name, dest, msg) => {
+      const entity = dest.find(
         p =>
           p.name.localeCompare(name, undefined, { sensitivity: "base" }) === 0
-      );
-      if (entity) {
-        message.channel.send(l.__("TRACK.ALREADY_TRACKED"));
-        return;
-      }
-      if (dest.length >= limit) {
-        message.channel.send(l.__("TRACK.LIMIT_REACHED", { limit }));
-        return;
-      }
-
-      entity = entityList.find(
-        p =>
-          p.Name.localeCompare(name, undefined, { sensitivity: "base" }) === 0
       );
       if (!entity) {
         message.channel.send(l.__("TRACK.NOT_FOUND"));
         return;
       }
-      dest.push({ id: entity.Id, name: entity.Name });
+
+      dest.splice(dest.indexOf(entity), 1);
       if (!(await setConfig(guild))) {
         message.channel.send(l.__("CONFIG_NOT_SET"));
       }
-      message.channel.send(l.__(msg, { name: entity.Name }));
+      message.channel.send(l.__(msg, { name: entity.name }));
     };
 
-    const results = await search(name);
     switch (type) {
     case "player":
-      return track(
-        results.players,
+      return untrack(
+        name,
         guild.config.trackedPlayers,
-        "TRACK.PLAYER_TRACKED",
-        30
+        "TRACK.PLAYER_UNTRACKED"
       );
     case "guild":
-      return track(
-        results.guilds,
+      return untrack(
+        name,
         guild.config.trackedGuilds,
-        "TRACK.GUILD_TRACKED"
+        "TRACK.GUILD_UNTRACKED"
       );
     case "alliance":
-      return track(
-        results.alliances,
+      return untrack(
+        name,
         guild.config.trackedAlliances,
-        "TRACK.ALLIANCE_TRACKED",
-        1
+        "TRACK.ALLIANCE_UNTRACKED"
       );
     }
   }
--- queries/search.js
+++ queries/search.js
@@ -4,9 +4,11 @@ const SEARCH_ENDPOINT = "https://gameinfo.albiononline.com/api/gameinfo/search";
 
 exports.search = async q => {
   try {
+    console.log(`Searching entities in Albion Online for: ${q}`);
     const res = await axios.get(SEARCH_ENDPOINT, { params: { q } });
     return res.data;
   } catch (e) {
+    console.log(`Failed to search entities in API: ${e}`);
     return {
       players: [],
       guilds: [],
