576870609293a453268f0aac21c15d743f2c6a54
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-03-06 18:29:46 -0400

    Add test command
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+const { getI18n } = require("../messages");
+
+module.exports = {
+  aliases: ["test"],
+  description: "HELP.TEST",
+  run: async (client, guild, message) => {
+    const l = getI18n(guild);
+
+    if (!guild.config.channel) {
+      message.channel.send(l.__("CHANNEL_NOT_SET"));
+    } else {
+      try {
+        const channel = client.channels.find(
+          c => c.id === guild.config.channel
+        );
+        await channel.send(l.__("TEST_MSG", { channel }));
+      } catch (e) {
+        message.channel.send(l.__("TEST_FAIL"));
+      }
+    }
+  }
+};
