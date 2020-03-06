2a8c595977e649b87040973935aa54d8db2ca6b8
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-03-06 18:29:34 -0400

    Add some checks for channel command
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- commands/channel.js
+++ commands/channel.js
@@ -14,6 +14,17 @@ module.exports = {
     }
     const channel = message.mentions.channels.first();
 
+    if (channel.type !== "text") {
+      message.channel.send(l.__("CHANNEL.NOT_FOUND"));
+      return;
+    }
+
+    // Check if client can see channel
+    if (!client.channels.some(c => c.id === channel.id)) {
+      message.channel.send(l.__("CHANNEL.NOT_FOUND"));
+      return;
+    }
+
     guild.config.channel = channel.id;
     if (!(await setConfig(guild))) {
       message.channel.send(l.__("CONFIG_NOT_SET"));
