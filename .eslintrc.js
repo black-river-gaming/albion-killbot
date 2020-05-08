ef1e0199f4cf04c2a1942bab89ab21dbeb44a101
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-05-08 05:17:12 -0400

    Remove line-break rule
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     },
     rules: {
         indent: ["error", 2],
-        "linebreak-style": ["error", "unix"],
         quotes: ["error", "double"],
         semi: ["error", "always"]
     }
