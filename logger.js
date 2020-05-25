78ca3f81ad76b9d99333f8b7baad502ad1e030b8
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-05-25 01:39:56 -0400

    Add loggly integration
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- logger.js
+++ logger.js
@@ -1,5 +1,4 @@
 const { createLogger, format, transports } = require("winston");
-const logdna = require("logdna-winston");
 const path = require("path");
 
 const logger = createLogger({
@@ -27,12 +26,25 @@ if (process.env.NODE_ENV === "development") {
 }
 
 if (process.env.LOGDNA_KEY) {
+  const logdna = require("logdna-winston");
   logger.add(
     new logdna({
       key: process.env.LOGDNA_KEY,
       app: "Albion Killbot",
       env: process.env.NODE_ENV || "unset",
       index_meta: true
+    })
+  );
+}
+
+if (process.env.LOGGLY_KEY) {
+  const { Loggly } = require("winston-loggly-bulk");
+  logger.add(
+    new Loggly({
+      token: process.env.LOGGLY_KEY,
+      subdomain: "albion-killbot",
+      tags: ["Winston-NodeJS"],
+      json: true
     })
   );
 }
