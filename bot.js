a5b5d673f3701a58c55cfb11ba0a7aaf13d82236
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-07-31 18:24:54 -0400

    Code cleanup on bot code
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- bot.js
+++ bot.js
@@ -18,60 +18,6 @@ const client = new Discord.Client({
 });
 client.commands = commands;
 
-const getDefaultChannel = guild => {
-  // get "original" default channel
-  if (guild.channels.has(guild.id)) return guild.channels.get(guild.id);
-
-  // Check for a "general" channel, which is often default chat
-  const generalChannel = guild.channels.find(channel => channel.name === "general");
-  if (generalChannel) return generalChannel;
-  // Now we get into the heavy stuff: first channel in order where the bot can speak
-  // hold on to your hats!
-  return guild.channels
-    .filter(c => c.type === "text" && c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-    .sort((a, b) => a.position - b.position)
-    .first();
-};
-
-const msgErrors = {};
-const sendGuildMessage = async (guild, message, type = "general") => {
-  if (!guild.config) guild.config = await config.getConfig(guild);
-
-  let channelId = guild.config.channel[type];
-  if (!channelId) channelId = guild.config.channel.general;
-  // Old structure backport
-  if (typeof(guild.config.channel) === "string") channelId = guild.config.channel;
-
-  const l = messages.getI18n(guild);
-  let channel = client.channels.find(c => c.id === channelId);
-  if (!channel) {
-    logger.warn(`Channel not configured for guild ${guild.name}.`);
-    channel = getDefaultChannel(guild);
-  }
-  if (!channel) return;
-  try {
-    await channel.send(message);
-    msgErrors[guild.id] = 0;
-  } catch (e) {
-    logger.error(`Unable to send message to guild ${guild.name}/${channel.name}: ${e}`);
-
-    if (
-      e.code === Discord.Constants.APIErrors.UNKNOWN_CHANNEL ||
-      e.code === Discord.Constants.APIErrors.MISSING_ACCESS ||
-      e.code === Discord.Constants.APIErrors.MISSING_PERMISSIONS
-    ) {
-      if (!msgErrors[guild.id]) msgErrors[guild.id] = 0;
-      msgErrors[guild.id]++;
-      // If more than 50 msg errors occur in succession, bot will leave and warn owner
-      if (msgErrors[guild.id] > 50) {
-        logger.warn(`Leaving guild ${guild.name} due to excessive message errors. Warning owner.`);
-        await guild.owner.send(l.__("LEAVE", { guild: guild.name }));
-        await guild.leave();
-      }
-    }
-  }
-};
-
 client.on("ready", async () => {
   logger.info(`Connected to Discord as ${client.user.tag}`);
 });
@@ -117,10 +63,10 @@ client.on("message", async message => {
 client.on("guildCreate", async guild => {
   logger.info(`Joined guild "${guild.name}". Creating default settings.`);
   guild.config = await config.getConfig(guild);
-  guild.config.channel = getDefaultChannel(guild).id;
+  guild.config.channel = exports.getDefaultChannel(guild).id;
   config.setConfig(guild);
   const l = messages.getI18n(guild);
-  sendGuildMessage(guild, l.__("JOIN"));
+  exports.sendGuildMessage(guild, l.__("JOIN"));
 });
 
 client.on("guildDelete", guild => {
@@ -128,7 +74,59 @@ client.on("guildDelete", guild => {
   config.deleteConfig(guild);
 });
 
-exports.sendGuildMessage = sendGuildMessage;
+exports.getDefaultChannel = guild => {
+  // get "original" default channel
+  if (guild.channels.has(guild.id)) return guild.channels.get(guild.id);
+
+  // Check for a "general" channel, which is often default chat
+  const generalChannel = guild.channels.find(channel => channel.name === "general");
+  if (generalChannel) return generalChannel;
+  // Now we get into the heavy stuff: first channel in order where the bot can speak
+  // hold on to your hats!
+  return guild.channels
+    .filter(c => c.type === "text" && c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+    .sort((a, b) => a.position - b.position)
+    .first();
+};
+
+const msgErrors = {};
+exports.sendGuildMessage = async (guild, message, type = "general") => {
+  if (!guild.config) guild.config = await config.getConfig(guild);
+
+  let channelId = guild.config.channel[type];
+  if (!channelId) channelId = guild.config.channel.general;
+  // Old structure backport
+  if (typeof(guild.config.channel) === "string") channelId = guild.config.channel;
+
+  const l = messages.getI18n(guild);
+  let channel = client.channels.find(c => c.id === channelId);
+  if (!channel) {
+    logger.warn(`Channel not configured for guild ${guild.name}.`);
+    channel = exports.getDefaultChannel(guild);
+  }
+  if (!channel) return;
+  try {
+    await channel.send(message);
+    msgErrors[guild.id] = 0;
+  } catch (e) {
+    logger.error(`Unable to send message to guild ${guild.name}/${channel.name}: ${e}`);
+
+    if (
+      e.code === Discord.Constants.APIErrors.UNKNOWN_CHANNEL ||
+      e.code === Discord.Constants.APIErrors.MISSING_ACCESS ||
+      e.code === Discord.Constants.APIErrors.MISSING_PERMISSIONS
+    ) {
+      if (!msgErrors[guild.id]) msgErrors[guild.id] = 0;
+      msgErrors[guild.id]++;
+      // If more than 50 msg errors occur in succession, bot will leave and warn owner
+      if (msgErrors[guild.id] > 50) {
+        logger.warn(`Leaving guild ${guild.name} due to excessive message errors. Warning owner.`);
+        await guild.owner.send(l.__("LEAVE", { guild: guild.name }));
+        await guild.leave();
+      }
+    }
+  }
+};
 
 exports.run = async token => {
   database.connect();
