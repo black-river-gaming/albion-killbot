ed1ce0913aff65dcc59cbde2fa69781575563c9e
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-07-31 17:27:50 -0400

    Improve channel command to split by categories
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- commands/channel.js
+++ commands/channel.js
@@ -1,13 +1,31 @@
 const { setConfig } = require("../config");
 const { getI18n } = require("../messages");
 
+const categories = ["general", "events", "battles", "rankings"];
+
 module.exports = {
   aliases: ["channel"],
-  args: ["channel"],
+  args: ["channel", "category"],
   description: "HELP.CHANNEL",
-  run: async (client, guild, message) => {
+  run: async (client, guild, message, args) => {
     const l = getI18n(guild.config.lang);
 
+    if (!guild.config.channel.general) {
+      guild.config.channel = {};
+    }
+
+    // Show current settings if no args are given
+    if (!args[0] && !args[1]) {
+      const categories = Object.keys(guild.config.channel);
+      if (categories.length === 0) return message.channel.send(l.__("CHANNEL_NOT_SET"));
+      for (let category of categories) {
+        const channel = client.channels.find(c => c.id === guild.config.channel[category]);
+        await message.channel.send(l.__("CHANNEL.SET_CHANNEL", { category, channel }));
+      }
+      return;
+    }
+
+    // Validate mentioned channel first
     if (!message.mentions.channels || !message.mentions.channels.first()) {
       message.channel.send(l.__("CHANNEL.NO_CHANNEL"));
       return;
@@ -25,11 +43,17 @@ module.exports = {
       return;
     }
 
-    guild.config.channel = channel.id;
+    // Validate category
+    const category = args[1].toLowerCase() || "general";
+    if (categories.indexOf(category) < 0) {
+      return message.channel.send(l.__("CHANNEL.CATEGORIES_AVAILABLE", { categories: categories.join(", ") }));
+    }
+
+    guild.config.channel[category] = channel.id;
     if (!(await setConfig(guild))) {
       message.channel.send(l.__("CONFIG_NOT_SET"));
     }
 
-    message.channel.send(l.__("CHANNEL.SET_CHANNEL", { channel: channel }));
+    message.channel.send(l.__("CHANNEL.SET_CHANNEL", { category, channel }));
   },
 };
