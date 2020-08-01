9686c303a302cc029af3b8eb979b1d08fdf79656
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-08-01 17:45:40 -0400

    Update test comamnd to test all channels
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- commands/test.js
+++ commands/test.js
@@ -6,16 +6,22 @@ module.exports = {
   run: async (client, guild, message) => {
     const l = getI18n(guild.config.lang);
 
-    if (!guild.config.channel) {
+    if (!guild.config.channel || typeof guild.config.channel === "string") {
+      guild.config.channel = {};
+    }
+
+    if (Object.keys(guild.config.channel).length === 0) {
       message.channel.send(l.__("CHANNEL_NOT_SET"));
     } else {
-      try {
-        const channel = client.channels.find(
-          c => c.id === guild.config.channel,
-        );
-        await channel.send(l.__("TEST_MSG", { channel }));
-      } catch (e) {
-        message.channel.send(l.__("TEST_FAIL"));
+      const categories = Object.keys(guild.config.channel);
+      if (categories.length === 0) return message.channel.send(l.__("CHANNEL_NOT_SET"));
+      for (let category of categories) {
+        const channel = client.channels.find(c => c.id === guild.config.channel[category]);
+        try {
+          await channel.send(l.__("TEST_MSG", { channel }));
+        } catch (e) {
+          message.channel.send(l.__("TEST_FAIL", { channel }));
+        }
       }
     }
   },
