65d43a1a26c97279c56af6cd1c7f09729335e0aa
Agnaldo Junior <agnaldo.junior01@gmail.com>
2020-09-06 14:56:41 -0400

    Change job consuming to be stack pop
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- queries/events.js
+++ queries/events.js
@@ -188,22 +188,15 @@ exports.scan = async ({ client, sendGuildMessage }) => {
   await Promise.all(
     Array(NOTIFY_JOBS)
       .fill()
-      .map((_, i) => {
-        const chunk_size = notifiedGuildIds.length / NOTIFY_JOBS;
-        return notifiedGuildIds.slice(i * chunk_size, (i + 1) * chunk_size);
-      })
-      .map(async (guildIds, i) => {
-        if (guildIds.length == 0) return;
-        logger.debug(`Running notify job #${i} with ${guildIds.length} guilds.`);
-        for (let guildId of guildIds) {
-          const guild = client.guilds.get(guildId);
+      .map(async (_, i) => {
+        while (notifiedGuildIds.length > 0) {
+          const guild = client.guilds.get(notifiedGuildIds.pop());
           guild.config = allGuildConfigs[guild.id];
           if (!guild.config || !eventsByGuild[guild.id]) continue;
-
           const newEventsCount = eventsByGuild[guild.id].length;
           if (newEventsCount > 0) {
             logger.info(`[Job #${i}] Sending ${newEventsCount} new events to guild "${guild.name}"`);
-          }
+          } else continue;
 
           for (let event of eventsByGuild[guild.id]) {
             dailyRanking.add(guild, event, allGuildConfigs[guild.id]);
@@ -221,8 +214,9 @@ exports.scan = async ({ client, sendGuildMessage }) => {
               await sendGuildMessage(guild, embedEvent(event, guild.config.lang), "events");
             }
           }
+          logger.debug(`#${notifiedGuildIds.length} guilds remaining to notify.`);
         }
-        logger.debug(`Notify job #${i} done.`);
       }),
   );
+  logger.info("Scan Events finished.");
 };
