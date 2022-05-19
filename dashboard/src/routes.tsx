6e7fbdaec47bceff3ad88d24a53fc3efa0281d24
LucianoSilva <luciano_sa_@hotmail.com>
2022-05-19 01:13:04 -0300

    Create routing system for Dashboard.

--- dashboard/src/routes.tsx
+++ dashboard/src/routes.tsx
@@ -1,1 +1,27 @@
-export {};
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+
+interface IRedirectTo {
+  redirectTo: string;
+}
+
+function ProtectedRoutes({ redirectTo }: IRedirectTo) {
+  const isAuthenticated = false;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />;
+}
+
+function MainRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+
+      <Route element={<ProtectedRoutes redirectTo="/" />}>
+        <Route path="/example" element={<div>Example</div>} />
+      </Route>
+
+      <Route path="*" element={<h1>404 - Not found</h1>}></Route>
+    </Routes>
+  );
+}
+
+export default MainRoutes;
---
+++ dashboard/src/shared/utils.ts
@@ -0,0 +1,1 @@
+export const getLocalStorageItem = localStorage.getItem("token");
