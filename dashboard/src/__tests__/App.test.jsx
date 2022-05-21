cc7543b3fea9be72452d0894e68f12e2f230c374
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-05-21 05:31:29 -0400

    Add initial tests
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

---
+++ dashboard/src/__tests__/App.test.jsx
@@ -0,0 +1,7 @@
+import App from "App";
+import renderer from "react-test-renderer";
+
+it("renders correctly", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
