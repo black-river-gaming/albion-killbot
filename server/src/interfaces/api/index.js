e9a945a8cb685592509e4da0babe492ec41d6e61
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-05-21 16:48:40 -0400

    Add simple express app.
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- server/src/interfaces/api/index.js
+++ server/src/interfaces/api/index.js
@@ -1,7 +1,13 @@
 const logger = require("../../helpers/logger");
+const api = require("./api");
+
+const { PORT } = process.env;
+const port = PORT || 80;
 
 async function run() {
   logger.info(`Starting Api...`);
+  await api.listen(port);
+  logger.verbose(`Api is listening on port ${port}.`);
 }
 
 async function cleanup(e) {
