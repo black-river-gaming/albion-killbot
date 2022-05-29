4e09ca42bd17d8055466656118e2d4444bb63794
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-05-29 06:13:32 -0400

    Implement refresh token on api

--- server/src/ports/discord.js
+++ server/src/ports/discord.js
@@ -4,6 +4,11 @@ async function getToken(code) {
   return await discordApiClient.exchangeCode(code);
 }
 
+async function refreshToken(refreshToken) {
+  return await discordApiClient.refreshToken(refreshToken);
+}
+
 module.exports = {
   getToken,
+  refreshToken,
 };
