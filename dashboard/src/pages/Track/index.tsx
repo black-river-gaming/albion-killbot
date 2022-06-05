34bda3edc64645d38091e5f39f484fd7640e36c4
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-06-05 18:37:18 -0400

    Initial track implementation

--- dashboard/src/pages/Track/index.tsx
+++ dashboard/src/pages/Track/index.tsx
@@ -1,5 +1,41 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { Button, Card, Form, InputGroup } from "react-bootstrap";
+
 const Track = () => {
-  return <h1>Track</h1>;
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e?.preventDefault();
+    console.log(`Perform search for: ${search}`);
+  };
+
+  return (
+    <Card className="p-2">
+      <h4 className="d-flex justify-content-center">Track</h4>
+      <Form onSubmit={handleSearch}>
+        <Form.Group controlId="search-albion" className="p-3">
+          <Form.Label>Search</Form.Label>
+          <InputGroup>
+            <Form.Control
+              type="text"
+              aria-describedby="search-help"
+              placeholder="Search in Albion Online for name or id"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button variant="primary" type="submit">
+              <FontAwesomeIcon icon={faSearch} />
+            </Button>
+          </InputGroup>
+          <Form.Text id="search-help" muted>
+            For alliances, input the alliance id
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </Card>
+  );
 };
 
 export default Track;
