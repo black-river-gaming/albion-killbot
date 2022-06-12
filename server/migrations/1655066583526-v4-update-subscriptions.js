ad97d6519d862b8af1280d6f0a3735fb704ec795
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-06-12 17:44:59 -0400

    Add migration to new subscription system

---
+++ server/migrations/1655066583526-v4-update-subscriptions.js
@@ -0,0 +1,33 @@
+const database = require("../src/ports/database");
+const SUBSCRIPTIONS_COLLECTION = "subscriptions";
+const SETTINGS_COLLECTION = "settings";
+
+async function exportSubscriptionsToCollection() {
+  const settingsCollection = database.getCollection(SETTINGS_COLLECTION);
+  const subscriptionsCollection = database.getCollection(SUBSCRIPTIONS_COLLECTION);
+  const settings = await settingsCollection.find({});
+
+  let setting = await settings.next();
+  while (setting) {
+    if (setting.subscription) {
+      const { subscription, guild } = setting;
+      await subscriptionsCollection.insertOne({ ...subscription, guild });
+      await settingsCollection.updateOne(
+        { _id: setting._id },
+        {
+          $unset: { subscription: "" },
+        },
+      );
+    }
+
+    setting = await settings.next();
+  }
+}
+
+async function run() {
+  await exportSubscriptionsToCollection();
+}
+
+module.exports = {
+  run,
+};
