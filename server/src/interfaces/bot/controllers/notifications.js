c9b5b770c400e0728caa993ecfcf4918cb28d0e9
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-08-11 14:14:38 -0400

    Fix #125

--- server/src/interfaces/bot/controllers/notifications.js
+++ server/src/interfaces/bot/controllers/notifications.js
@@ -5,10 +5,7 @@ const NOTIFICATION_TIMEOUT = 10000;
 
 async function sendNotification(client, channelId, message) {
   const channel = client.channels.cache.find((c) => c.id === channelId);
-  if (!channel) {
-    // Notify server owner in case of deleted channel?
-    return logger.debug(`${channel.guild.name}/${channelId}: Channel not found.`);
-  }
+  if (!channel) return;
   if (!channel.guild) channel.guild = { name: "Unknown Guild" };
 
   logger.debug(`Sending notification to ${channel.guild.name}/#${channel.name}:`, { metadata: message.embeds });
