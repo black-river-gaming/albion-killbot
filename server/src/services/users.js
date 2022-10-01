42411436bf788d91cc9c758d3b4058cf3c90c2e0
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-10-01 00:12:17 -0400

    Move getServer to servers services

--- server/src/services/users.js
+++ server/src/services/users.js
@@ -10,24 +10,6 @@ async function getCurrentUser(accessToken) {
   }
 }
 
-async function getCurrentUserServers(accessToken) {
-  try {
-    const botServerIds = (await discord.getBotGuilds()).map((s) => s.id);
-    const servers = await discord.getMeGuilds(accessToken);
-
-    return servers
-      .filter((server) => server.owner || server.admin)
-      .map((server) => {
-        server.bot = botServerIds.indexOf(server.id) >= 0;
-        return server;
-      });
-  } catch (e) {
-    logger.error(`Error while retrieving user guilds:`, e);
-    throw e;
-  }
-}
-
 module.exports = {
   getCurrentUser,
-  getCurrentUserServers,
 };
