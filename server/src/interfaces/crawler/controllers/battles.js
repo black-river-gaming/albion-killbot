e5976a97a30ebcae254687ed60b5b50fdd41cca4
Agnaldo Junior <agnaldo.junior01@gmail.com>
2022-12-22 19:51:38 -0400

    crawler: Add the ability to publish events/battles in batch

--- server/src/interfaces/crawler/controllers/battles.js
+++ server/src/interfaces/crawler/controllers/battles.js
@@ -1,6 +1,8 @@
 const logger = require("../../../helpers/logger");
 const { fetchBattlesTo, publishBattle } = require("../../../services/battles");
 
+const { AMQP_QUEUE_BATTLES_BATCH } = process.env;
+
 let latestBattle;
 
 async function fetchBattles() {
@@ -14,16 +16,29 @@ async function fetchBattles() {
   if (battles.length === 0) return logger.verbose("No new battles.");
 
   // Publish new battles, from oldest to newest
+  const battlesToPublish = [];
   logger.verbose(`Publishing ${battles.length} new battles to exchange...`);
   for (const batl of battles) {
     if (latestBattle && batl.id <= latestBattle.id) {
       logger.warn(`The published id is lower than latestBattle! Skipping.`);
       continue;
     }
-    logger.debug(`Publishing battle ${batl.id}`);
-    await publishBattle(batl);
+
+    if (!AMQP_QUEUE_BATTLES_BATCH) {
+      logger.debug(`Publishing battle ${batl.id}`);
+      await publishBattle(batl);
+    } else {
+      battlesToPublish.push(batl);
+    }
+
     latestBattle = batl;
   }
+
+  // Batch publish
+  if (AMQP_QUEUE_BATTLES_BATCH) {
+    await publishBattle(battlesToPublish);
+  }
+
   logger.info("Publish battles complete.");
 }
 
