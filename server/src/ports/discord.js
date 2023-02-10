3a077ab0dfb6e75a7ffcdc5ec0ddd1583c6725f8
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-02-10 19:56:18 -0400

    Add memoize functions to discord API calls

--- server/src/ports/discord.js
+++ server/src/ports/discord.js
@@ -1,5 +1,6 @@
 const discordApiClient = require("./adapters/discordApiClient");
 const discordHelper = require("../helpers/discord");
+const { memoize } = require("../helpers/cache");
 
 const { DISCORD_TOKEN } = process.env;
 
@@ -12,40 +13,68 @@ async function refreshToken(refreshToken) {
 }
 
 async function getUser(accessToken) {
-  const user = await discordApiClient.getCurrentUser(`Bearer ${accessToken}`);
-  return discordHelper.transformUser(user);
+  return memoize(`discord.users.${accessToken}`, async () => {
+    const user = await discordApiClient.getCurrentUser(`Bearer ${accessToken}`);
+    return discordHelper.transformUser(user);
+  });
 }
 
 async function getUserGuilds(accessToken, params) {
-  const guilds = await discordApiClient.getCurrentUserGuilds(`Bearer ${accessToken}`, params);
-  return guilds.map(discordHelper.transformGuild);
+  return memoize(
+    `discord.users.${accessToken}.guilds`,
+    async () => {
+      const guilds = await discordApiClient.getCurrentUserGuilds(`Bearer ${accessToken}`, params);
+      return guilds.map(discordHelper.transformGuild);
+    },
+    {
+      timeout: 60000,
+    },
+  );
 }
 
 async function getBotGuilds() {
-  let foundAll = false;
-  let after;
-  const guilds = [];
+  return memoize(
+    `discord.botGuilds`,
+    async () => {
+      let foundAll = false;
+      let after;
+      const guilds = [];
 
-  // Iterate over all pages of guilds because bot can join more than the default 200 servers limit
-  while (!foundAll) {
-    const guildList = await discordApiClient.getCurrentUserGuilds(`Bot ${DISCORD_TOKEN}`, { limit: 200, after });
-    guilds.push(...guildList);
+      // Iterate over all pages of guilds because bot can join more than the default 200 servers limit
+      while (!foundAll) {
+        const guildList = await discordApiClient.getCurrentUserGuilds(`Bot ${DISCORD_TOKEN}`, { limit: 200, after });
+        guilds.push(...guildList);
 
-    after = guilds[guilds.length - 1].id;
-    if (guildList.length < 200) foundAll = true;
-  }
+        after = guilds[guilds.length - 1].id;
+        if (guildList.length < 200) foundAll = true;
+      }
 
-  return guilds.map(discordHelper.transformGuild);
+      return guilds.map(discordHelper.transformGuild);
+    },
+    {
+      timeout: 30000,
+    },
+  );
 }
 
 async function getGuild(guildId) {
-  const guild = await discordApiClient.getGuild(`Bot ${DISCORD_TOKEN}`, guildId);
-  return discordHelper.transformGuild(guild);
+  return memoize(`discord.guilds.${guildId}`, async () => {
+    const guild = await discordApiClient.getGuild(`Bot ${DISCORD_TOKEN}`, guildId);
+    return discordHelper.transformGuild(guild);
+  });
 }
 
 async function getGuildChannels(guildId) {
-  const channels = await discordApiClient.getGuildChannels(`Bot ${DISCORD_TOKEN}`, guildId);
-  return channels.map(discordHelper.transformChannel);
+  return memoize(
+    `discord.guilds.${guildId}.channels`,
+    async () => {
+      const channels = await discordApiClient.getGuildChannels(`Bot ${DISCORD_TOKEN}`, guildId);
+      return channels.map(discordHelper.transformChannel);
+    },
+    {
+      timeout: 30000,
+    },
+  );
 }
 
 async function leaveGuild(guildId) {
