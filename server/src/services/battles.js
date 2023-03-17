f11a654982834a780e83df10e0a63f9a0b8d3833
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-03-17 04:06:01 -0400

    Add new server param to service layer

--- server/src/services/battles.js
+++ server/src/services/battles.js
@@ -7,7 +7,7 @@ const { sleep } = require("../helpers/scheduler");
 const BATTLES_EXCHANGE = "battles";
 const BATTLES_QUEUE_PREFIX = "battles";
 
-async function fetchBattlesTo(latestBattle, { offset = 0 } = {}, battles = []) {
+async function fetchBattlesTo(latestBattle, { server, offset = 0 } = {}, battles = []) {
   // Maximum offset reached, just return what we have
   if (offset >= 1000) return battles;
 
@@ -15,12 +15,14 @@ async function fetchBattlesTo(latestBattle, { offset = 0 } = {}, battles = []) {
     // If not latestBattle, just fetch a single one to create a reference
     if (!latestBattle) {
       return await albion.getBattles({
+        server,
         limit: 1,
       });
     }
 
-    logger.verbose(`Fetching battles with offset: ${offset}`);
+    logger.verbose(`[${server}] Fetching battles with offset: ${offset}`);
     const albionBattles = await albion.getBattles({
+      server,
       offset,
     });
 
@@ -28,17 +30,19 @@ async function fetchBattlesTo(latestBattle, { offset = 0 } = {}, battles = []) {
       if (batl.id <= latestBattle.id) return false;
       // Ignore items already on the queue
       if (battles.findIndex((e) => e.id === batl.id) >= 0) return true;
+      // Set battle server for later
+      batl.server = server;
       battles.push(batl);
       return true;
     });
 
     return foundLatest
       ? battles.sort((a, b) => a.id - b.id)
-      : fetchBattlesTo(latestBattle, { offset: offset + albionBattles.length }, battles);
+      : fetchBattlesTo(latestBattle, { server, offset: offset + albionBattles.length }, battles);
   } catch (err) {
-    logger.warn(`Unable to fetch battle data from API [${err}]. Retrying...`);
+    logger.warn(`[${server}] Unable to fetch battle data from API [${err}]. Retrying...`);
     await sleep(5000);
-    return fetchBattlesTo(latestBattle, { offset }, battles);
+    return fetchBattlesTo(latestBattle, { server, offset }, battles);
   }
 }
 
@@ -51,8 +55,8 @@ async function subscribeBattles(callback, { queue_suffix } = {}) {
   return await subscribe(BATTLES_EXCHANGE, queue, callback);
 }
 
-async function getBattle(battleId) {
-  return await albion.getBattle(battleId);
+async function getBattle(battleId, { server }) {
+  return await albion.getBattle(battleId, { server });
 }
 
 module.exports = {
--- server/src/services/events.js
+++ server/src/services/events.js
@@ -7,7 +7,7 @@ const { sleep } = require("../helpers/scheduler");
 const EVENTS_EXCHANGE = "events";
 const EVENTS_QUEUE_PREFIX = "events";
 
-async function fetchEventsTo(latestEvent, { offset = 0 } = {}, events = []) {
+async function fetchEventsTo(latestEvent, { server, offset = 0 } = {}, events = []) {
   // Maximum offset reached, just return what we have
   if (offset >= 1000) return events;
 
@@ -15,12 +15,14 @@ async function fetchEventsTo(latestEvent, { offset = 0 } = {}, events = []) {
     // If not latestEvent, just fetch a single one to create a reference
     if (!latestEvent) {
       return await albion.getEvents({
+        server,
         limit: 1,
       });
     }
 
-    logger.verbose(`Fetching events with offset: ${offset}`);
+    logger.verbose(`[${server}] Fetching events with offset: ${offset}`);
     const albionEvents = await albion.getEvents({
+      server,
       offset,
     });
 
@@ -28,17 +30,19 @@ async function fetchEventsTo(latestEvent, { offset = 0 } = {}, events = []) {
       if (evt.EventId <= latestEvent.EventId) return false;
       // Ignore items already on the queue
       if (events.findIndex((e) => e.EventId === evt.EventId) >= 0) return true;
+      // Set event server for later
+      evt.server = server;
       events.push(evt);
       return true;
     });
 
     return foundLatest
       ? events.sort((a, b) => a.EventId - b.EventId)
-      : fetchEventsTo(latestEvent, { offset: offset + albionEvents.length }, events);
-  } catch (err) {
-    logger.warn(`Unable to fetch event data from API [${err}]. Retrying...`);
+      : fetchEventsTo(latestEvent, { server, offset: offset + albionEvents.length }, events);
+  } catch (error) {
+    logger.warn(`[${server}] Unable to fetch event data from API: ${error.message}. Retrying...`, { error });
     await sleep(5000);
-    return fetchEventsTo(latestEvent, { offset }, events);
+    return fetchEventsTo(latestEvent, { server, offset }, events);
   }
 }
 
@@ -51,12 +55,12 @@ async function subscribeEvents(callback, { queue_suffix } = {}) {
   return await subscribe(EVENTS_EXCHANGE, queue, callback);
 }
 
-async function getEvent(eventId) {
-  return await albion.getEvent(eventId);
+async function getEvent(eventId, { server }) {
+  return await albion.getEvent(eventId, { server });
 }
 
-async function getEventVictimLootValue(event) {
-  return albion.getLootValue(event);
+async function getEventVictimLootValue(event, { server }) {
+  return albion.getLootValue(event, { server });
 }
 
 module.exports = {
