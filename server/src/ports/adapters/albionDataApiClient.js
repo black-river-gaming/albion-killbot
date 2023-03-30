fdb8f424362c92270bf51030ce012d8b851f047f
Agnaldo junior <agnaldo.junior01@gmail.com>
2023-03-30 01:38:40 -0400

    feat: Fetch loot value from East server (#199)
    Fetch loot value from east server

--- server/src/ports/adapters/albionDataApiClient.js
+++ server/src/ports/adapters/albionDataApiClient.js
@@ -1,11 +1,32 @@
 const axios = require("axios");
+const { SERVERS } = require("../../helpers/constants");
+
+const ALBION_SERVERS = [
+  {
+    name: SERVERS.WEST,
+    url: "https://www.albion-online-data.com/api/v2/stats/",
+  },
+  {
+    name: SERVERS.EAST,
+    url: "https://east.albion-online-data.com/api/v2/stats/",
+  },
+];
 
 const albionDataApiClient = axios.create({
-  baseURL: "https://www.albion-online-data.com/api/v2/stats/",
+  baseURL: ALBION_SERVERS[0].url,
 });
 
-async function getPrices(itemList, { locations, qualities }) {
+albionDataApiClient.interceptors.request.use((config) => {
+  if (config.server) {
+    const server = ALBION_SERVERS.find((server) => server.name === config.server);
+    if (server) config.baseURL = server.url;
+  }
+  return config;
+});
+
+async function getPrices(itemList, { server, locations, qualities }) {
   const res = await albionDataApiClient.get(`Prices/${itemList}.json`, {
+    server,
     params: {
       locations,
       qualities,
