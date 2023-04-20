1a774d2aa136bc7197b6187fc32ff23dfbb68fff
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-04-20 08:45:55 -0400

    Update redacted paths

--- server/src/helpers/logger.js
+++ server/src/helpers/logger.js
@@ -8,7 +8,13 @@ const { MODE, DEBUG_LEVEL, LOGGLY_TOKEN, LOGGLY_SUBDOMAIN } = process.env;
 const level = DEBUG_LEVEL || "info";
 
 const redact = fastRedact({
-  paths: ["notification.files", "response.request", "error.requestBody.files", "error.config.headers.Authorization"],
+  paths: [
+    "notification.files",
+    "error.requestBody.files",
+    "response.request",
+    "config.headers.Authorization",
+    "response.config.headers.Authorization",
+  ],
   serialize: false,
   strict: false,
 });
