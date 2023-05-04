5706c33bbe6e819d2d417fd80d3597590b31218c
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-05-04 05:20:59 -0400

    Add healthcheck endpoint

--- server/src/interfaces/api/api.js
+++ server/src/interfaces/api/api.js
@@ -13,6 +13,10 @@ const app = express();
 
 const { NODE_ENV, RATE_LIMIT_WINDOW = 60000, RATE_LIMIT_REQUESTS = 0 } = process.env;
 
+app.get("/healthz", (req, res) => {
+  return res.send("healthy");
+});
+
 // This is supposed to run behind an nginx reverse proxy so trust it
 app.enable("trust proxy");
 
