6a5c601f268ae475f2e9573305e10c5571c95a36
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-06-12 16:24:29 -0400

    Add migration to mitigate threshold db problems

---
+++ server/migrations/1686601142550-add-battles-threshold.js
@@ -0,0 +1,40 @@
+const logger = require("../src/helpers/logger");
+const database = require("../src/ports/database");
+
+async function run() {
+  const collection = database.getCollection("settings");
+  const settings = await collection.find({});
+
+  while (await settings.hasNext()) {
+    const setting = await settings.next();
+
+    if (setting.battles.threshold) {
+      logger.debug(`Skipping setting ${setting._id}`, { setting });
+      continue;
+    }
+
+    try {
+      await collection.updateOne(
+        { _id: setting._id },
+        {
+          $set: {
+            battles: {
+              ...setting.battles,
+              threshold: {
+                players: 0,
+                guilds: 0,
+                alliances: 0,
+              },
+            },
+          },
+        },
+      );
+    } catch (error) {
+      logger.error(`Unable to update setting ${setting._id}: ${error.message}. Skipping.`, { setting, error });
+    }
+  }
+}
+
+module.exports = {
+  run,
+};
