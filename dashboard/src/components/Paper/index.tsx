d50edbbb329b460ac86afeef0c9d46a88c4ae735
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-01-26 12:41:25 -0400

    Get rid of HomePage styles

--- dashboard/src/components/Paper/index.tsx
+++ dashboard/src/components/Paper/index.tsx
@@ -1,14 +1,19 @@
+import React from "react";
 import PaperStyles from "./styles";
 
 interface PaperProps {
   elevation?: number;
   className?: string;
   children?: JSX.Element | JSX.Element[] | string | number;
+  style?: React.CSSProperties;
 }
 
-const Paper = ({ elevation = 2, className, children }: PaperProps) => {
+const Paper = ({ elevation = 2, className, children, style }: PaperProps) => {
   return (
-    <PaperStyles className={`elevation-${elevation} ${className}`}>
+    <PaperStyles
+      className={`elevation-${elevation} ${className}`}
+      style={style}
+    >
       {children}
     </PaperStyles>
   );
