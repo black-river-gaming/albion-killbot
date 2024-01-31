60c3a621f29282b765ea81eb5f21ad50037eac4a
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-01-31 09:21:10 -0400

    Enable on production

--- server/src/config/production.js
+++ server/src/config/production.js
@@ -3,7 +3,7 @@ const { getBool } = require("../helpers/utils");
 module.exports = {
   features: {
     events: {
-      displayTraitIcons: getBool(process.env.DISPLAY_TRAIT_ICONS, false),
+      displayTraitIcons: getBool(process.env.DISPLAY_TRAIT_ICONS, true),
       useHistoryPrices: getBool(process.env.FEATURE_USE_HISTORY_PRICES, true),
     },
   },
