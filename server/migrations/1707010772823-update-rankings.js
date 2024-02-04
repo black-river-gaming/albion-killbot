fb5d87bc17fb0dd4365ab6467f3800c5784bf02e
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-02-03 21:52:44 -0400

    Write migration to update to new ranking settings

---
+++ server/migrations/1707010772823-update-rankings.js
@@ -0,0 +1,51 @@
+const logger = require("../src/helpers/logger");
+const database = require("../src/ports/database");
+
+async function run() {
+  const collection = database.getCollection("settings");
+  const settings = await collection.find({});
+
+  while (await settings.hasNext()) {
+    const setting = await settings.next();
+
+    if (setting.rankings && setting.rankings.daily && setting.rankings.weekly && setting.rankings.monthly) {
+      logger.debug(`Skipped server: ${setting.server}`, { setting });
+      continue;
+    }
+
+    if (!setting.rankings) setting.rankings = {};
+    setting.rankings.daily = "off";
+    setting.rankings.weekly = "off";
+    setting.rankings.monthly = "off";
+
+    const convertRanking = (old) =>
+      ({
+        off: "off",
+        houly: "1hour",
+        daily: "1day",
+      }[old] || old);
+    if (setting.rankings.pvpRanking) {
+      setting.rankings.daily = convertRanking(setting.rankings.pvpRanking);
+      delete setting.rankings.pvpRanking;
+    }
+    delete setting.rankings.guildRanking;
+
+    try {
+      await collection.updateOne(
+        { _id: setting._id },
+        {
+          $set: {
+            rankings: setting.rankings,
+          },
+        },
+      );
+      logger.debug(`Updated server ${setting.server}`, { setting });
+    } catch (error) {
+      logger.error(`Unable to update srver ${setting.server}: ${error.message}. Skipping.`, { setting });
+    }
+  }
+}
+
+module.exports = {
+  run,
+};
