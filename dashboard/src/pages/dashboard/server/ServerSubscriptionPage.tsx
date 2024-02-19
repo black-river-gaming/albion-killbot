f81e6e539be8267117bfb6e6ac4bee0a6601f60e
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-02-19 17:43:19 -0400

    Start rework on ServerSubscriptionPage

--- dashboard/src/pages/dashboard/server/ServerSubscriptionPage.tsx
+++ dashboard/src/pages/dashboard/server/ServerSubscriptionPage.tsx
@@ -1,30 +1,64 @@
 import Loader from "components/Loader";
+import Box from "components/common/Box";
 import NoData from "components/common/NoData";
 import SubscriptionStripePriceCard from "components/subscriptions/SubscriptionStripePriceCard";
-import { Card } from "react-bootstrap";
+import { Card, Stack } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { useFetchServerQuery } from "store/api";
-import { useFetchSubscriptionPricesQuery } from "store/api/subscriptions";
+import {
+  useCreateSubscriptionCheckoutMutation,
+  useFetchSubscriptionPricesQuery,
+} from "store/api/subscriptions";
 
 const ServerSubscriptionPage = () => {
   const { serverId = "" } = useParams();
   const server = useFetchServerQuery(serverId);
   const prices = useFetchSubscriptionPricesQuery({ currency: "usd" });
 
-  if (!server.data) return <NoData />;
+  const [dispatchCreateSubscriptionCheckout, createSubscriptionCheckout] =
+    useCreateSubscriptionCheckoutMutation();
 
+  if (createSubscriptionCheckout.isLoading) return <Loader />;
+  if (createSubscriptionCheckout.isSuccess && createSubscriptionCheckout.data) {
+    window.location.href = createSubscriptionCheckout.data.url;
+  }
+
+  if (!server.data) return <NoData />;
   const { subscription } = server.data;
 
   if (!subscription) {
     return (
       <div>
-        <h5 className="d-flex justify-content-center">
-          Ascend to Power - Unlock Premium Benefits
-        </h5>
         {prices.isLoading && <Loader />}
-        {prices.data?.prices.map((price) => (
-          <SubscriptionStripePriceCard price={price} />
-        ))}
+        {prices.data && (
+          <Box>
+            <h5 className="d-flex justify-content-center">
+              Ascend to Power - Unlock Premium Benefits
+            </h5>
+            <div
+              className="pb-2"
+              style={{
+                overflowX: "auto",
+                overflowY: "hidden",
+              }}
+            >
+              <Stack direction="horizontal" gap={4}>
+                {prices.data.prices.map((price) => (
+                  <SubscriptionStripePriceCard
+                    key={price.id}
+                    price={price}
+                    onSelect={() => {
+                      dispatchCreateSubscriptionCheckout({
+                        priceId: price.id,
+                        serverId,
+                      });
+                    }}
+                  />
+                ))}
+              </Stack>
+            </div>
+          </Box>
+        )}
       </div>
     );
   }
