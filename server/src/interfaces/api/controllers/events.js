e27bb9bd8a73ed0859f015bd45ed430cc3485935
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-03-11 03:55:57 -0400

    Fix api lootValue changes

--- server/src/interfaces/api/controllers/events.js
+++ server/src/interfaces/api/controllers/events.js
@@ -18,8 +18,9 @@ async function getEventImage(req, res) {
   const event = await eventsService.getEvent(eventId, { server });
   if (!event) return res.sendStatus(404);
 
-  const lootValue = await eventsService.getEventVictimLootValue(event, { server });
-  const eventImage = await generateEventImage(event, { lootValue });
+  event.lootValue = await eventsService.getEventVictimLootValue(event, { server });
+  const eventImage = await generateEventImage(event);
+
   return res.set("Content-Disposition", `inline; filename="${event.EventId}-event.png"`).type("png").send(eventImage);
 }
 
@@ -29,11 +30,12 @@ async function getEventInventoryImage(req, res) {
 
   const event = await eventsService.getEvent(eventId, { server });
   if (!event) return res.sendStatus(404);
+
   const inventory = event.Victim.Inventory.filter((i) => i != null);
   if (!inventory || inventory.length === 0) return res.sendStatus(404);
 
-  const lootValue = await eventsService.getEventVictimLootValue(event, { server });
-  const eventInventoryImage = await generateInventoryImage(event, { lootValue });
+  event.lootValue = await eventsService.getEventVictimLootValue(event, { server });
+  const eventInventoryImage = await generateInventoryImage(event);
 
   return res
     .set("Content-Disposition", `inline; filename="${event.EventId}-inventory.png"`)
